fd2c269777af8453f61f019ff660351fb50bde44
Marcus Pereira <marcus@negros.dev>
2022-09-07 07:27:44 -0300

    Faker

--- __tests__/home_blueprint_test.py
+++ __tests__/home_blueprint_test.py
@@ -1,4 +1,6 @@
 from ward import test
+from faker import Faker
+from flask import url_for
 
 from app import db
 from app.models import Post
@@ -7,26 +9,27 @@ from __tests__.fixtures import browser
 
 @test('página principal deve está online')
 def _(browser=browser):
-    browser.visit('/')    
+    browser.visit(url_for('home.index'))    
 
     assert browser.is_text_present('Olá, Marcus Pereira')
 
 
 @test('Visitante acessa a página principal e vê posts')
 def _(browser=browser):
-    post = Post(title='Curtindo as ferias em Salvador', published=True,
+    faker = Faker()
+    post = Post(title=faker.paragraph(), published=True,
                 text='text...')
     db.session.add(post)
     db.session.commit()
-        
-    browser.visit('/')
 
-    assert browser.is_text_present('Curtindo as ferias em Salvador')
+    browser.visit(url_for('home.index'))
+
+    assert browser.is_text_present(post.title)
 
 
 @test('Visitante não consegue visualizar posts')
 def _(browser=browser):
-    browser.visit('/')
+    browser.visit(url_for('home.index'))
 
     assert browser.is_text_present('Nenhum post cadastrado')
 
