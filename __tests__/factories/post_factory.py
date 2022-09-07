b7906808767d48254d0a7e9205834b04dbb5aeb4
Marcus Pereira <marcus@negros.dev>
2022-09-07 07:34:25 -0300

    Factories

---
+++ __tests__/factories/post_factory.py
@@ -0,0 +1,17 @@
+import factory
+from faker import Faker
+
+from app import db
+from app.models import Post
+
+faker = Faker()
+
+class PostFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Post
+        sqlalchemy_session = db.session
+        sqlalchemy_session_persistence = "commit"
+
+    title = faker.text()
+    text = faker.paragraph()
+    published = True
--- __tests__/home_blueprint_test.py
+++ __tests__/home_blueprint_test.py
@@ -1,9 +1,7 @@
 from ward import test
-from faker import Faker
 from flask import url_for
 
-from app import db
-from app.models import Post
+from __tests__.factories.post_factory import PostFactory
 from __tests__.fixtures import browser
 
 
@@ -16,11 +14,7 @@ def _(browser=browser):
 
 @test('Visitante acessa a página principal e vê posts')
 def _(browser=browser):
-    faker = Faker()
-    post = Post(title=faker.paragraph(), published=True,
-                text='text...')
-    db.session.add(post)
-    db.session.commit()
+    post = PostFactory.create()
 
     browser.visit(url_for('home.index'))
 
