9c6c1ca8e2025c0352024d1c3b825c416e8a769c
Marcus Pereira <marcus@negros.dev>
2022-09-12 08:04:46 -0300

    Carregando extensions via Dynaconf

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 db = SQLAlchemy()
 
 def init_app(app):
     db.init_app(app)
     Migrate(app, db)
+
+    @app.shell_context_processor
+    def load_context_processor():
+        from app.models import Post
+
+        return dict(
+            app=app,
+            db=db,
+            Post=Post,
+        )
